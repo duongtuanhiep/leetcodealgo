88064f968acdb54337c81d63de98bdcda57434a2
Duong Hiep <48057648+duongtuanhiep@users.noreply.github.com>
2025-07-06 01:46:18 +0300

    pep8 guideline

---
+++ oop/pep8.py
@@ -0,0 +1,23 @@
+"""
+Naming conventions:
+- Functions, variables, and methods: lowercase_with_underscores (snake_case).
+- Classes: CapitalizedWords (CamelCase).
+- Constants: ALL_UPPERCASE_WITH_UNDERSCORES.
+
+Whitespace:
+- Avoid extraneous spaces inside parentheses, brackets, or braces.
+- No spaces before commas, semicolons, or colons.
+- Use a single space after commas, semicolons, or colons.
+= No extra whitespace at the end of lines or before argument lists.
+
+Line breaks:
+- Prefer breaking lines inside parentheses, brackets, or braces rather than using backslashes.
+- When breaking around binary operators, place the operator at the beginning of the new line.
+
+Programming recommendations:
+- Avoid extraneous whitespace at the end of lines.
+- Use blank lines to separate logical sections of code.
+- Prefer consistency within a project, even over strict adherence to PEP 8, if it improves readability.
+
+General principle: Code is read much more often than it is written; prioritize readability and maintainability.
+"""
